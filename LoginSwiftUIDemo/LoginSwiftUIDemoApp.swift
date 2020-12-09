e936b238eaaced5c81d9b441332706c2a4ff8789
Aleksandar Geyman <ageyman@appolica.com>
2020-12-09 08:49:44 +0200

    initial commit

---
+++ LoginSwiftUIDemo/LoginSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSwiftUIDemoApp.swift
+//  LoginSwiftUIDemo
+//
+//  Created by Aleksandar Geyman on 9.12.20.
+//
+
+import SwiftUI
+
+@main
+struct LoginSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
